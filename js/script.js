3bb657ef896faf676e71529e10ee79810afa8f2d
Maxwell <lubalecliff@gmail.com>
2023-10-24 15:49:24 +0300

    changed to a better API

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
 document.getElementById('chatbot-form').addEventListener('submit', async (event) => {
-    event.preventDefault(); // Add this line to prevent the form from refreshing the page
+    event.preventDefault(); // Prevent the form from refreshing the page
 
     const userInput = document.getElementById('userInput').value;
 
-    const url = 'https://robomatic-ai.p.rapidapi.com/api';
+    const url = 'https://stileschat-ai.p.rapidapi.com/api';
     const options = {
         method: 'POST',
         headers: {
-            'content-type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/json',
             'X-RapidAPI-Key': '83814eee54msh26db8fe720703dep1bf139jsndb623b73feac',
-            'X-RapidAPI-Host': 'robomatic-ai.p.rapidapi.com'
+            'X-RapidAPI-Host': 'stileschat-ai.p.rapidapi.com',
         },
-        body: new URLSearchParams({
-            in: userInput,
-            op: 'in',
-            cbot: '1',
-            SessionID: 'RapidAPI1',
-            cbid: '1',
-            key: 'RHMN5hnQ4wTYZBGCF3dfxzypt68rVP',
-            ChatSource: 'RapidAPI',
-            duration: '1'
+        body: JSON.stringify({
+            promptInput: userInput,
+            context: false
         })
     };
-    
-
+     
     try {
         const response = await fetch(url, options);
         const result = await response.text();
 
-       // Parse the JSON response and extract the "out" text
-const responseObj = JSON.parse(result);
-const outText = responseObj.out;
+        // Assuming the response is plain text, you can display it in the HTML
+        const chatbotResponseDiv = document.getElementById('chatbot-response');
+        chatbotResponseDiv.textContent = result;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
+
+
+      
 
 // Display the "out" text in the HTML, for example, in a div with the ID 'chatbot-response'
 // const chatbotResponseDiv = document.getElementById('chatbot-response');
@@ -49,8 +49,4 @@ chatbotResponseDiv.textContent = jokeWithoutLineBreaks;
 
         
         
-    } catch (error) {
-        console.error('Error:', error);
-    }
-});
-
+    
